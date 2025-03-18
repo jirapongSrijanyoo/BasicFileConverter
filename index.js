c4b101da0b5310b68863ace0ad974c8727402b1d
loma_0531 <jirapong.srijan@gmail.com>
2025-03-18 22:28:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const rl = readline.createInterface({
 function generateTimestampFilename(extension) {
     const now = new Date();
     const day = String(now.getDate()).padStart(2, '0');
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // เดือนเริ่มจาก 0
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
     const year = now.getFullYear();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
